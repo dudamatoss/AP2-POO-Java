5fd970799c9a33dac298250e14301d3e34ad5709
dudamatos <dudasantosmatos15@gmail.com>
2025-05-14 22:46:03 -0300

    Update: add functions in controllers

--- src/main/java/com/POO/AP2/controllers/CarroController.java
+++ src/main/java/com/POO/AP2/controllers/CarroController.java
@@ -1,4 +1,60 @@
 package com.POO.AP2.controllers;
 
+import com.POO.AP2.model.Carro;
+import com.POO.AP2.services.CarroService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/carros")
 public class CarroController {
+    private CarroService carroService;
+
+    public CarroController(CarroService carroService) {
+        this.carroService = carroService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Carro>> ListarTodosCarros() {
+        return ResponseEntity
+                .ok()
+                .body(this.carroService.ListarTodosCarros());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Carro> ListarPorId (@PathVariable int id) {
+        return ResponseEntity
+                .ok(this.carroService.ListarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Carro> CriarCarro(@RequestBody Carro carroRequest) {
+        Carro carro = this.carroService.CriarCarro(carroRequest);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(carro.getId()).toUri();
+
+        return ResponseEntity
+                .created(uri)
+                .body(carro);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Carro> EditarCarro(@RequestBody Carro carroRequest, @PathVariable int id) {
+        this.carroService.EditarCarro(carroRequest, id);
+        return ResponseEntity
+                .ok()
+                .build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> DeletarCarro(@PathVariable int id) {
+        this.carroService.ListarPorId(id);
+        return  ResponseEntity
+                .ok()
+                .build();
+    }
 }
