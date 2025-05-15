cb037e5bae172ab3fe4ce8ec1226eec89d9bd66c
dudamatos <dudasantosmatos15@gmail.com>
2025-05-14 22:44:48 -0300

    Update: add functions in services

--- src/main/java/com/POO/AP2/services/CarroService.java
+++ src/main/java/com/POO/AP2/services/CarroService.java
@@ -1,4 +1,36 @@
 package com.POO.AP2.services;
 
+import com.POO.AP2.model.Carro;
+import com.POO.AP2.repositories.CarroRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CarroService {
+    private CarroRepository carroRepository;
+
+    public CarroService(CarroRepository carroRepository) {
+        this.carroRepository = carroRepository;
+    }
+
+    public List<Carro> ListarTodosCarros(){
+        return this.carroRepository.ListarTodosCarros();
+    }
+
+    public Carro ListarPorId (int id) {
+        return this.carroRepository.ListarPorId(id);
+    }
+
+    public Carro CriarCarro (Carro carroRequest){
+        return this.carroRepository.CriarCarro(carroRequest);
+    }
+
+    public Carro EditarCarro (Carro carroRequest, int id){
+        return this.carroRepository.EditarCarro(carroRequest, id);
+    }
+
+    public void DeleteCarro (int id){
+        this.carroRepository.DeleteCarro(id);
+    }
 }
